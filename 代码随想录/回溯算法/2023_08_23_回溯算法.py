2126431d7cdf6f6655ed7b7a51dd372ee2985145
Suncheng2022 <15613125247@163.com>
2023-08-23 23:53:33 +0800

    5道 小狮子回来啦！

--- 代码随想录/回溯算法/2023_08_23_回溯算法.py
+++ 代码随想录/回溯算法/2023_08_23_回溯算法.py
@@ -52,32 +52,81 @@ class Solution:
 
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         """ 491.递增子序列
-            子序列  不能重复选 {2,1} {1,2}视为相同，因此需要startIndex
-                   去重 通过调试运行发现的[但此题万万不可排序] """
+                1.很明显不能重复使用元素，所以使用startIndex控制下层递归元素的起始索引
+                2.同一层使用过的元素不能重复使用，注意 是 同一层, 因此要确定used的位置 """
         def backtracking(startInd):
             if len(path) > 1:
                 res.append(path[:])
             if startInd >= len(nums):
                 return
-
+            used = set()        # 本层元素不能重复用 否则会产生相同结果
             for i in range(startInd, len(nums)):
                 if (path and path[-1] > nums[i]) or nums[i] in used:
                     continue
+                used.add(nums[i])
                 path.append(nums[i])
-                used.append(nums[i])
                 backtracking(i + 1)
-                used.pop()
                 path.pop()
 
         res = []
         path = []
-        used = []
         backtracking(0)
         return res
+
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        """ 90.子集II """
+        def backtracking(startIndex):
+            res.append(path[:])
+            if startIndex >= len(nums):
+                return
+            for i in range(startIndex, len(nums)):
+                if i > 0 and nums[i - 1] == nums[i] and not used[i - 1]:    # 同层使用过相同元素，跳过
+                    continue
+                used[i] = True
+                path.append(nums[i])
+                backtracking(i + 1)
+                path.pop()
+                used[i] = False
+
+        res = []
+        path = []
+        used = [False] * len(nums)
+        nums.sort()
+        backtracking(0)
+        return res
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        """ 78.子集 """
+        def backtracking(startIndex):
+            res.append(path[:])
+            if startIndex >= len(nums):
+                return
+            for i in range(startIndex, len(nums)):
+                path.append(nums[i])
+                backtracking(i + 1)
+                path.pop()
+
+        res = []
+        path = []
+        backtracking(0)
+        return res
+
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        """ 93.复原IP地址 """
+        # def backtracking(startIndex):
+        #     if len(path) == 4:
+        #         res.append(".".join(path))
+        #         return
+        #     for i in range(startIndex, len(nums)):
+        #
+        #
+        # res = []
+        # path = []
+
 
 
 if __name__ == '__main__':
     sl = Solution()
 
-    nums = [4,6,7,7]
-    print(sl.findSubsequences(nums))
+    nums = [1]
+    print(sl.subsets(nums))
