edeaf7619050c658e503145ce604edf3b67867b0
Suncheng <15613125247@163.com>
2023-08-23 20:40:41 +0800

    第2轮 回溯算法 结束

--- 代码随想录/回溯算法/2023_08_19_回溯算法.py
+++ 代码随想录/回溯算法/2023_08_19_回溯算法.py
@@ -326,8 +326,32 @@ class Solution:
         backtracking()
         return res
 
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        """ 47.全排列II """
+        def backtracking():
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if i > 0 and nums[i - 1] == nums[i] and not used[i - 1]:
+                    continue
+                if not used[i]:
+                    path.append(nums[i])
+                    used[i] = True
+                    backtracking()
+                    path.pop()
+                    used[i] = False
+
+        res = []
+        path = []
+        used = [False] * len(nums)
+        nums.sort()
+        backtracking()
+        return res
+
+
 if __name__ == "__main__":
     sl = Solution()
 
     nums = [1,2,3]
-    print(sl.permute(nums))
+    print(sl.permuteUnique(nums))
