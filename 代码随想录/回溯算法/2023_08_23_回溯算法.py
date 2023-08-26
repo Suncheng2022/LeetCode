f3f9270a3670e80cd90c946eebcc851deed9d508
Suncheng2022 <15613125247@163.com>
2023-08-26 17:05:49 +0800

    7道 I kissed the little lion. ^_^

--- 代码随想录/回溯算法/2023_08_23_回溯算法.py
+++ 代码随想录/回溯算法/2023_08_23_回溯算法.py
@@ -113,20 +113,170 @@ class Solution:
 
     def restoreIpAddresses(self, s: str) -> List[str]:
         """ 93.复原IP地址 """
-        # def backtracking(startIndex):
-        #     if len(path) == 4:
-        #         res.append(".".join(path))
-        #         return
-        #     for i in range(startIndex, len(nums)):
-        #
-        #
-        # res = []
-        # path = []
+        def isValid(s):
+            if len(s) == 0 or (len(s) > 1 and s[0] == '0') or not 0 <= int(s) <= 255:
+                return False
+            return True
+        def backtracking(startInd):
+            if startInd == len(s) and len(path) == 4:
+                res.append(".".join(path))
+                return
+            for i in range(startInd, len(s)):
+                if not isValid(s[startInd:i + 1]):
+                    continue
+                path.append(s[startInd:i + 1][:])
+                backtracking(i + 1)
+                path.pop()
 
+        res = []
+        path = []
+        backtracking(0)
+        return res
+
+    def partition(self, s: str) -> List[List[str]]:
+        """ 131.分割回文串 """
+        def isValid(s):
+            l, r = 0, len(s) - 1
+            while l <= r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        def backtracking(startInd):
+            if startInd >= len(s):
+                res.append(path[:])
+                return
+            for i in range(startInd, len(s)):
+                if not isValid(s[startInd:i + 1]):
+                    continue
+                path.append(s[startInd:i + 1][:])
+                backtracking(i + 1)
+                path.pop()
+
+        res = []
+        path = []
+        backtracking(0)
+        return res
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        """ 40.数组总和II """
+        def backtracking(startInd):
+            if sum(path) == target:
+                res.append(path[:])
+                return
+            for i in range(startInd, len(candidates)):
+                if i > 0 and candidates[i - 1] == candidates[i] and not used[i - 1]:
+                    continue
+                if sum(path) > target:
+                    break
+                path.append(candidates[i])
+                used[i] = True
+                backtracking(i + 1)
+                path.pop()
+                used[i] = False
+
+        res = []
+        path = []
+        used = [False] * len(candidates)
+        candidates.sort()
+        backtracking(0)
+        return res
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        """ 39.数组总和 """
+        def backtracking(startInd):
+            if sum(path) == target:
+                res.append(path[:])
+                return
+            for i in range(startInd, len(candidates)):
+                if sum(path) + candidates[i] > target:      # 剪枝，而不应该只写 sum(path) > target.
+                    break
+                path.append(candidates[i])
+                backtracking(i)
+                path.pop()
+
+        res = []
+        path = []
+        backtracking(0)
+        return res
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        """ 17.电话号码的字母组合
+            回溯 模拟多层for循环
+            多个集合求组合，不使用startIndex
+            backtracking(index)的参数用来指示当前处理digits的第几个字符 """
+        if not len(digits):
+            return []
+        digit2letter = dict(zip(range(2, 10), ['abc', 'def',
+                                               'ghi', 'jkl', 'mno',
+                                               'pqrs', 'tuv', 'wxyz']))
+
+        def backtracking(index, path):
+            if index == len(digits):
+                res.append(path[:])
+                return
+            letters = digit2letter[int(digits[index])]
+            for i in range(0, len(letters)):
+                path += letters[i]
+                backtracking(index + 1, path)
+                path = path[:-1]
+
+        res = []
+        path = ""
+        backtracking(0, path)
+        return res
+
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        """ 216.数组总和III
+            递归参数：想一下回溯的树结构，同一层 不使用重复元素，否则产生相同结果
+            终止条件：k个数
+            遍历：就是for i in range(startInd, 10)  范围固定1~9
+            """
+        def backtracking(startInd):
+            if len(path) == k:
+                if sum(path) == n:
+                    res.append(path[:])
+                return
+            for i in range(startInd, 10):
+                if sum(path) + i > n:
+                    break
+                path.append(i)
+                backtracking(i + 1)
+                path.pop()
+
+        res = []
+        path = []
+        backtracking(1)
+        return res
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        """ 77.组合
+            一个集合求组合，使用startIndex """
+        def backtracking(startInd):
+            if len(path) == k:
+                res.append(path[:])
+                return
+            for i in range(startInd, n + 1):
+                # if len(path) + 1 > k:   # 这个剪枝效率太低
+                #     break
+                # 剪枝，效率更高
+                if i > n - (k - len(path)) + 1:     # 边界条件需要细想一下
+                    break
+                path.append(i)
+                backtracking(i + 1)
+                path.pop()
+
+        res = []
+        path = []
+        backtracking(1)
+        return res
 
 
 if __name__ == '__main__':
     sl = Solution()
 
-    nums = [1]
-    print(sl.subsets(nums))
+    k = 2
+    n = 4
+    print(sl.combine(n, k))
