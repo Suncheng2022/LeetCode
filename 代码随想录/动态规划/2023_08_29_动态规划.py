710fe4ee9da45e3d8bf3e213034c88e8d4c36203
Suncheng2022 <15613125247@163.com>
2023-09-12 00:34:07 +0800

    .5道

--- 代码随想录/动态规划/2023_08_29_动态规划.py
+++ 代码随想录/动态规划/2023_08_29_动态规划.py
@@ -658,12 +658,31 @@ class Solution:
                     dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 2)    # dp[i-1][j-1]+2等于dp[i-1][j]+1或dp[i][j-1]+1 ?
         return dp[-1][-1]
 
+    def minDistance(self, word1: str, word2: str) -> int:
+        """ 72.编辑距离 """
+
+
+
+        # m, n = len(word1), len(word2)
+        # dp = [[0] * (n + 1) for _ in range(m + 1)]      # dp[i][j] 以索引i-1元素结尾的word1子串、以索引j-1元素结尾的word2子串 的编辑距离
+        # for i in range(m + 1):
+        #     dp[i][0] = i
+        # for j in range(n + 1):
+        #     dp[0][j] = j
+        # for i in range(1, m + 1):
+        #     for j in range(1, n + 1):
+        #         if word1[i - 1] == word2[j - 1]:
+        #             dp[i][j] = dp[i - 1][j - 1]
+        #         else:
+        #             dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i][j - 1] + 1, dp[i - 1][j] + 1)
+        # return dp[-1][-1]
+
 
 if __name__ == '__main__':
     sl = Solution()
 
-    word1 = "sea"
-    word2 = "eat"
+    word1 = "intention"
+    word2 = "execution"
     print(sl.minDistance(word1, word2))
 
     """
