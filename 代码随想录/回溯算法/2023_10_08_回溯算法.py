c0fa0b85e73751cdd747c8699f8a7f394a353d01
Suncheng <suncheng@ict.ac.cn>
2023-10-09 15:14:08 +0800

    4道

--- 代码随想录/回溯算法/2023_10_08_回溯算法.py
+++ 代码随想录/回溯算法/2023_10_08_回溯算法.py
@@ -104,12 +104,142 @@ class Solution:
         backtracking(path, 0)
         return res
 
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        """ 39.组合总和
+            中等
+            求组合问题：
+                1.一个集合求组合，要使用startInd
+                2.多个集合求组合，不使用startInd
+            可重复取，递归函数的参数依然为i，表示可以重复取当前的数 """
+        # path = []
+        # res = []
+        #
+        # def backtracking(startInd):
+        #     if sum(path) >= target:
+        #         if sum(path) == target:
+        #             res.append(path[:])
+        #         return
+        #     for i in range(startInd, len(candidates)):
+        #         path.append(candidates[i])
+        #         backtracking(i)
+        #         path.pop()
+        #
+        # backtracking(0)
+        # return res
+
+        # 剪枝优化
+        # 通过sum(path)剪枝，符合剪枝直接return，因为提前nums.sort()了
+        path = []
+        res = []
+
+        def backtracking(startInd):
+            if sum(path) == target:
+                res.append(path[:])
+                return
+            for i in range(startInd, len(candidates)):
+                if sum(path) + candidates[i] > target:
+                    return
+                path.append(candidates[i])
+                backtracking(i)
+                path.pop()
+
+        candidates.sort()
+        backtracking(0)
+        return res
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        """ 40.组合总和II
+            一个集合求组合，递归函数使用startInd
+            元素不能重复使用，则递归函数参数为 i+1
+        39.组合总和 不包含重复元素，递归参数 i+1 即可
+        本题难点：candidates包含重复元素，且不能有重复结果——同层去重，则必须先排序 """
+        # 已针对target剪枝，剪枝需放在单层搜索中
+        # 时间：O(n*2^n)   空间：O(n)
+        path = []
+        res = []
+        used = [False] * len(candidates)
+
+        def backtracking(startInd, used):
+            if sum(path) == target:
+                res.append(path[:])
+                return
+            for i in range(startInd, len(candidates)):
+                if i > 0 and candidates[i - 1] == candidates[i] and used[i - 1] == False:
+                    continue
+                if sum(path) + candidates[i] > target:
+                    return
+                used[i] = True
+                path.append(candidates[i])
+                backtracking(i + 1, used)
+                path.pop()
+                used[i] = False
+
+        candidates.sort()
+        backtracking(0, used)
+        return res
+
+    def partition(self, s: str) -> List[List[str]]:
+        """ 131.分割回文串
+            中等
+            分割问题 类似 组合问题，解题逻辑有一点点绕
+            切割对象是一个，使用startInd保证不重复切 """
+        # 时间：O(n*2^n)   空间：O(n^2) 没明白
+        path = []
+        res = []
+
+        def isPal(s):
+            """ 判断是否回文串 """
+            i, j = 0, len(s) - 1
+            while i <= j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+
+        def backtracking(startInd):
+            if startInd == len(s):
+                res.append(path[:])
+                return
+            for i in range(startInd, len(s)):
+                if not isPal(s[startInd:i + 1]):
+                    continue
+                path.append(s[startInd:i + 1])
+                backtracking(i + 1)
+                path.pop()
+
+        backtracking(0)
+        return res
+
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        """ 93.复原IP地址
+            中等
+            分割问题
+            分割对象是一个集合，使用startInd防止重复切割 """
+        path = []
+        res = []
+
+        def backtracking(startInd):
+            if startInd == len(s) and len(path) == 4:
+                res.append(".".join(path))
+                return
+            for i in range(startInd, len(s)):
+                if (len(s[startInd:i + 1]) > 1 and s[startInd] == '0') or len(path) >= 4:
+                    continue
+                if 0 <= int(s[startInd:i + 1]) <= 255:
+                    path.append(s[startInd:i + 1])
+                    backtracking(i + 1)
+                    path.pop()
+
+        backtracking(0)
+        return res
+
 
 if __name__ == "__main__":
     sl = Solution()
 
-    digits = ""
-    print(sl.letterCombinations(digits))
+    s = "101023"
+    print(sl.restoreIpAddresses(s))
 
     """
     回溯三部曲：
