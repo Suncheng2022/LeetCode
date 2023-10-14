d4d4100b1096ced2a40e053a7dc8c6bf72ded96f
Suncheng2022 <15613125247@163.com>
2023-10-14 17:49:40 +0800

    2道

--- 代码随想录/二叉树/2023_10_13_二叉树.py
+++ 代码随想录/二叉树/2023_10_13_二叉树.py
@@ -7,6 +7,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         """ 144.二叉树的前序遍历
@@ -24,7 +25,35 @@ class Solution:
         # backtracking(root)
         # return res
 
+        # 重写递归
+        # res = []
+        #
+        # def backtracking(node):
+        #     if not node:
+        #         return
+        #     res.append(node.val)
+        #     backtracking(node.left)
+        #     backtracking(node.right)
+        #
+        # backtracking(root)
+        # return res
+
         # 迭代
+        # stack = []
+        # res = []
+        # if not root:
+        #     return res
+        # stack.append(root)
+        # while stack:
+        #     node = stack.pop()
+        #     res.append(node.val)
+        #     if node.right:
+        #         stack.append(node.right)
+        #     if node.left:
+        #         stack.append(node.left)
+        # return res
+
+        # 重写迭代
         stack = []
         res = []
         if not root:
@@ -55,9 +84,36 @@ class Solution:
         # backtracking(root)
         # return res
 
+        # 重写递归
+        # res = []
+        #
+        # def backtracking(node):
+        #     if not node:
+        #         return
+        #     backtracking(node.left)
+        #     res.append(node.val)
+        #     backtracking(node.right)
+        #
+        # backtracking(root)
+        # return res
+
         # 迭代
-        res = []
+        # res = []
+        # stack = []
+        # node = root
+        # while node or stack:
+        #     if node:
+        #         stack.append(node)
+        #         node = node.left
+        #     else:
+        #         node = stack.pop()
+        #         res.append(node.val)
+        #         node = node.right
+        # return res
+
+        # 重写迭代
         stack = []
+        res = []
         node = root
         while node or stack:
             if node:
@@ -85,9 +141,37 @@ class Solution:
         # backtracking(root)
         # return res
 
+        # 重写递归
+        # res = []
+        #
+        # def backtracking(node):
+        #     if not node:
+        #         return
+        #     backtracking(node.left)
+        #     backtracking(node.right)
+        #     res.append(node.val)
+        #
+        # backtracking(root)
+        # return res
+
         # 迭代 由 先序遍历的 中左右 -> 修改为 中右左 -> 反转遍历结果，得到后序遍历的 左右中
-        res = []
+        # res = []
+        # stack = []
+        # if not root:
+        #     return res
+        # stack.append(root)
+        # while stack:
+        #     node = stack.pop()
+        #     res.append(node.val)
+        #     if node.left:
+        #         stack.append(node.left)
+        #     if node.right:
+        #         stack.append(node.right)
+        # return res[::-1]
+
+        # 重写迭代
         stack = []
+        res = []
         if not root:
             return res
         stack.append(root)
@@ -95,17 +179,66 @@ class Solution:
             node = stack.pop()
             res.append(node.val)
             if node.left:
-                stack.append(node.left)
+                res.append(node.left)
             if node.right:
-                stack.append(node.right)
+                res.append(node.right)
+        return res[::-1]
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        """ 102.二叉树的层序遍历
+            中等 """
+        # queue = [root]      # 队列
+        # res = []
+        # while queue:
+        #     length = len(queue)
+        #     tmp = []
+        #     for _ in range(length):
+        #         node = queue.pop(0)
+        #         if not node:
+        #             continue
+        #         tmp.append(node.val)
+        #         queue.extend([node.left, node.right])
+        #     if tmp:
+        #         res.append(tmp)
+        # return res
+
+        # 再写一遍 层序遍历
+        queue = [root]
+        res = []
+        while queue:
+            length = len(queue)
+            tmp = []
+            for _ in range(length):
+                node = queue.pop(0)
+                if not node:
+                    continue
+                tmp.append(node.val)
+                queue.extend([node.left, node.right])
+            if tmp:
+                res.append(tmp)
+        return res
+
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        """ 107.二叉树的层序遍历II
+            中等 """
+        queue = [root]
+        res = []
+        while queue:
+            length = len(queue)
+            tmp = []
+            for _ in range(length):
+                node = queue.pop(0)
+                if not node:
+                    continue
+                tmp.append(node.val)
+                queue.extend([node.left, node.right])
+            if tmp:
+                res.append(tmp)
         return res[::-1]
 
 
 if __name__ == "__main__":
     sl = Solution()
-
-    root = [1, None, 2, 3]
-    print(sl.inorderTraversal(root))
 
     """
     递归三要素：
