78cf449f2e5f8862603140f07573170fadd796d9
Suncheng2022 <15613125247@163.com>
2024-01-01 23:20:00 +0800

    1 道

--- 代码随想录/动态规划/2024_01_01_动态规划.py
+++ 代码随想录/动态规划/2024_01_01_动态规划.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def fib(self, n: int) -> int:
         """ 509.斐波那契数 """
@@ -58,6 +61,27 @@ class Solution:
             dp[2] = tmp
         return dp[-1]
 
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        """ 746.使用最小花费爬楼梯 """
+        # 虽然自己写对了，还是看了一下《代码随想录》
+        # n = len(cost)               # 一共多少阶楼梯
+        # dp = [0] * (n + 1)          # dp[i] 爬到索引第i阶楼梯所需最小花费；得能爬到第n阶楼梯呀，所以索引要到n
+        # dp[0] = 0                   # 初始化dp[0] dp[1]
+        # dp[1] = 0
+        # for i in range(2, n + 1):
+        #     dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])       # 递推公式，到达第i阶 可以从第i-1阶、第i-2阶跳
+        # return dp[-1]
+
+        # 优化空间，虽然个人不太推荐，还是多会一个吧
+        n = len(cost)
+        dp = [0] * 2
+        dp[0] = 0
+        dp[1] = 0
+        for i in range(2, n + 1):
+            tmp = min(dp[1] + cost[i - 1], dp[0] + cost[i - 2])                   # 注意，dp长度为2了
+            dp[0] = dp[1]
+            dp[1] = tmp
+        return dp[-1]
 
 """
 动规五部曲：
