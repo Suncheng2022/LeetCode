051cbd3e354ab311e1f23c344d5daeba82ebc838
Suncheng <suncheng@ict.ac.cn>
2024-01-08 11:08:15 +0800

    2 道

--- 代码随想录/动态规划/2024_01_01_动态规划.py
+++ 代码随想录/动态规划/2024_01_01_动态规划.py
@@ -529,13 +529,33 @@ class Solution:
         return max([max(ls) for ls in dp])
 
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        """ 1143.最长公共子序列 """
+        """ 1143.最长公共子序列
+            '子序列'意味着'不连续' """
         m = len(text1)
         n = len(text2)
         dp = [[0] * (n + 1) for _ in range(m + 1)]          # dp[i][j] 以text1[i-1]、text2[j-1]结尾的 最长公共子序列长度
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                pass
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return max([max(ls) for ls in dp])
+
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        """ 1035.不相交的线
+            其实就是 1143.最长公共子序列 """
+        m = len(nums1)
+        n = len(nums2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if nums1[i - 1] == nums2[j - 1]:            # 明确dp[i][j]意义
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return max([max(ls) for ls in dp])
+
 
 """
 动规五部曲：
